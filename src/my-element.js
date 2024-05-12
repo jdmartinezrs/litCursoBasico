8042c7f76eac21c2d01fbd4e830fec3bdd338164
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-05-12 04:17:34 -0500

    feat: :lipstick: Carrusell  joined to composition

--- src/my-element.js
+++ src/my-element.js
@@ -20,10 +20,11 @@ class Myelement extends LitElement{
   .music-container {
     
     width: 300px;
-    height: 700px;
-   // background-color: red;
+    height: 800px;
+    background-color: lightgray;
     padding: 20px;
     color: black; 
+    
 }
 
 .top-chart {
@@ -31,6 +32,8 @@ class Myelement extends LitElement{
   justify-content: space-between;
   align-items: center;
   margin-bottom: 30px;
+  //background: yellow;
+ 
 }
 
 .album {
@@ -75,8 +78,6 @@ class Myelement extends LitElement{
 
   }
 
-
-
 .album__2{
   
   width: 130px;
@@ -87,7 +88,6 @@ class Myelement extends LitElement{
   justify-content: end;
   align-items:center;
   border-radius: 10px; 
-  
 
 }
 
@@ -154,10 +154,68 @@ border-radius: 10px;
 
   }
 
+.lista__canciones{
+  width: 300px;
+    display:flex;
+    flex-direction:row;
+    //background: blue;
+    justify-content: space-beetween;
+    
+  }
+
+.lista__canciones img{
+  width: 50px;
+  height:50px;
+  border-radius: 5px; 
+}
+
+.lista__canciones__li{
+  list-style: none;
+  line-height: 2px;
+  width: 190px;
+  padding-left:10px;
+
+ 
+}
+
+.lista__canciones1{
+  width: 300px;
+    display:flex;
+    //background: blue;
+    justify-content: space-beetween;
+    
+  }
+
+.lista__canciones__li1{
+    list-style:none;
+    padding-top:8px;
+    padding-left:3px;
+  }
 
 
 
 
+
+.titleyml{
+  width:
+  display:flex;
+  //background: green;
+  width: 300px;
+}
+
+.lista_canciones__a img{
+  width: 50px;
+  height:50px;
+  border-radius: 5px;
+}
+
+.carrusell{
+  //background: orange;
+  width:400px;
+  height:82px;
+  overflow-y: auto;
+  
+}
 
 
 
@@ -219,17 +277,67 @@ border-radius: 10px;
   <div class="album__4">
   
   <ul class="album__4__list">
-  <img src="/img/cc.png" ">
+  <img src="/img/cc.png">
       <li>Crystal Castles I</li>
       <li><small>Crystal Castles, 2008</small></li>
   </ul>
   </div>
 
 
-
-
+  <div class="titleyml">
+  <h4>You may like</h4>
   </div>
-</div>
+
+
+<div class="carrusell">
+
+  <div class="lista__canciones"> 
+  <img src="/img/uno.png">
+  <div class= "lista__canciones__li">  
+  <li><p>Static</p></li>
+  <li><p><small>Godspeed You, Black Emperor</small></p></li>
+    </div>
+    <div class=lista__canciones__li1>
+  <li>22:36</li>
+  <li><small>2001<small></li>
+    </div>
+  </div>
+
+  <div class="lista__canciones"> 
+  <img src="/img/hem.png">
+  <div class= "lista__canciones__li">  
+  <li><p>Static</p></li>
+  <li><p><small>Godspeed You, Black Emperor</small></p></li>
+    </div>
+    <div class=lista__canciones__li1>
+  <li>22:36</li>
+  <li><small>2001<small></li>
+    </div>
+  </div>
+
+
+
+
+ 
+
+
+
+
+  
+ 
+ 
+  
+  
+  <div class="lista_canciones__a">
+  <img src="/img/hem.png">
+  </div>
+  </div>
+     
+
+     
+
+
+
 </body>
     `
   }
